4317cabb75a878b75c0b3a1e276d8309b2b7bba1
Danyvi <vinci.dani@gmail.com>
2022-04-03 00:18:39 -0400

    REST controller created

---
+++ src/test/java/xyz/codeandlearn/players/PlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.codeandlearn.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
